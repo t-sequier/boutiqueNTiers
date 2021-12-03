d01bf025aa9a7887e2d173f7d3a61f683cc3cfa7
Remi-iut <remi.raffestin@iut-rodez.fr>
2021-12-03 10:36:37 +0100

    ajout dans form ajout client

--- AppClient/FormAddClient.Designer.cs
+++ AppClient/FormAddClient.Designer.cs
@@ -235,6 +235,7 @@
             this.button1.TabIndex = 19;
             this.button1.Text = "Valider";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // linkLabel2
             // 
--- AppClient/FormAddClient.cs
+++ AppClient/FormAddClient.cs
@@ -17,6 +17,43 @@ namespace AppClient
             InitializeComponent();
         }
 
-        //Todo : Quand interface finie, faire ws pour mettre les données et insert dans bdd
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //On récupère les valeurs entrés dans le form
+            String nom = tbNom.Text;
+            String prenom = tbPrenom.Text;
+            String numRue = tbNumAdr.Text;
+            String rue = tbRue.Text;
+            int cp = int.Parse(tbCpAdr.Text);
+            String email = tbEmail.Text;
+            String tel = tbTel.Text;
+            //Pour la civilite, on récupère la valeur du radio button selectionné:
+            //on créer la variable
+            String civ = "";
+            //on vérifie si c'est le bouton 1 ou 2 qui a été séléctionné
+            //puis on récupère la valeur correspondante
+            bool isChecked = radioHomme.Checked;
+            if (isChecked) {civ = radioHomme.Text; } else { civ = radioFemme.Text; }
+
+            //On vérifie si tous les champs on été complété (sinon affiche popup avec message erreur)
+            if (nom == "" || prenom == "" || numRue == "" || rue == "" || cp.ToString() == "" || email == "" || tel == "")
+            {
+
+            }
+
+            
+            //On affiche une popup pour confirmer la création (et on résumes les infos)
+            MessageBox.Show("Êtes-vous sûr de vouloir créer un nouveau client avec ses informations ?\n"
+                + "(nom : " + nom 
+                + ", prenom : " + prenom 
+                + ", numRue : " + numRue 
+                + ", rue : " + rue 
+                + ", cp : " + cp 
+                + ", email : " + email 
+                + ", tel : " + tel 
+                + ", civ : " + civ + ")"
+                ,"Infos dev", MessageBoxButtons.YesNo);
+        }
+
     }
 }
